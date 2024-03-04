8f74f2714492660c27055ed72e2d15e712eb04fd
morpheuseses <matvey.igoshev@yandex.ru>
2024-03-05 00:15:14 +0500

    has made some other labs_2

--- docker/Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM php:8.0-apache
-WORKDIR /var/www/html
-RUN apt-get update -y && apt-get install -y libmariadb-dev
-RUN docker-php-ext-install mysqli
--- docker/src/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    $server = new \PDO("mysql:host=localhost;dbname=Pryazha;", 'root', 'EmPelmeny123');
-    if ($server) {
-        echo "Hello";
-    }
-?>
